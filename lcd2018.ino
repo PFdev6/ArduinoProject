808cc4e937f644a6cbc5a121c23ccaa554b4ad1d
PFdev6 <fedorovich2012bel@gmail.com>
2018-09-06 18:42:59 +0300

    Changed

--- lcd2018.ino
+++ lcd2018.ino
@@ -10,15 +10,10 @@ void setup() {
   // put your setup code here, to run once:
   lcd.begin();
   lcd.backlight();
-  lcd.print("D");
-  lcd.print("R");
-  lcd.print("O");
-  lcd.print("C");
-  lcd.print("H");
+  lcd.print("G");
   lcd.print("I");
-  lcd.print("L");
-  lcd.print("?");
-  lcd.print(")");
+  lcd.print("T");
+ 
 }
 
 void loop() {
